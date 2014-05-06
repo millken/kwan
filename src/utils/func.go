d3af0aa1786167f163af765f80c896561dfa57fb
millken <millken@gmail.com>
2014-05-06 18:45:04 +0800

    add cc stronger

--- src/utils/func.go
+++ src/utils/func.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
-	"fmt"
+	//"fmt"
 	"math/rand"
+	"math/big"
+	"strconv"
+	"encoding/base64"
 )
 
 func RandomString(size int) string {
@@ -19,10 +22,35 @@ func RandomString(size int) string {
 
 }
 
-func ToUnicode(str string) (result string) {
-	result = ""
-	for _, c := range str {
-		result += fmt.Sprintf("\\x%X", c)
+func unicode(input string) []int32 {
+	var result []int32
+	for _, c:= range input {
+		result = append(result, c)
 	}
-	return
+	return result
 }
+
+func Crypt(input , key string) string {
+	key2 := unicode(key)
+	key_len := len(key2)
+	result := ""
+	i := 0
+	for _, c := range input {
+		k := key2[i % key_len]
+		ck := new(big.Int).Xor(big.NewInt(int64(c)),big.NewInt(int64(k)))
+		cki, _ := strconv.Atoi(ck.String())
+		result += string(rune(cki))
+		i++
+	}
+	return result
+}
+
+func Base64_encode(str string) string {
+	return base64.StdEncoding.EncodeToString([]byte(str))
+}
+
+func Base64_decode(str string) string {
+	data, _ := base64.StdEncoding.DecodeString(str)
+	return string(data)
+}
+//http://play.golang.org/p/gmYKrUcx4D
--- src/webfilter/ddos.go
+++ src/webfilter/ddos.go
@@ -11,10 +11,12 @@ import (
 	"sync/atomic"
 	"time"
 	"utils"
+	"math/rand"
 )
 
 const (
-	DDOS_JS_FUNC = 1
+	DDOS_JS_REDIRECT = 10
+	DDOS_JS_TIMEOUT = 11
 	DDOS_FLASH   = 2
 	DDOS_CODE    = 3
 )
@@ -110,8 +112,18 @@ func (df DdosFilter) getDdosBody(uri *url.URL, key string, mode int32) (body str
 	q.Set("_xko", key)
 	uri.RawQuery = q.Encode()
 	switch mode {
-	case DDOS_JS_FUNC:
-		body = `<html><script>window.top.location = "` + utils.ToUnicode(uri.RequestURI()) + `";</script></html>`
+	case DDOS_JS_REDIRECT:
+		body = `<html><script>window.top.location = "` + uri.RequestURI() + `";</script></html>`
+	case DDOS_JS_TIMEOUT:
+		r := rand.Intn(7) + 1
+		rkey := utils.RandomString(r)
+		a := utils.Base64_encode(utils.Crypt(uri.RequestURI(), rkey))
+		body = `<html><script>
+		var B={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",d:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+String.fromCharCode(chr1);if(enc3!=64){output=output+String.fromCharCode(chr2)};if(enc4!=64){output=output+String.fromCharCode(chr3)}};output=B._d(output);return output},_d:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++}else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2}else{c2=utftext.charCodeAt(i+1);c3=utftext.charCodeAt(i+2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3}};return string}}
+		var A=B.d("`+ a +`"),C="`+rkey+`",O="";
+		for(var i=0;i<A.length;i++){var k = C.charCodeAt(i%C.length);O += String.fromCharCode(A.charCodeAt(i)^k);}
+		window.setTimeout(function(){window.top.location= O;}, 600);
+		</script></html>`
 	default:
 		body = "the site was been attacked!"
 	}
